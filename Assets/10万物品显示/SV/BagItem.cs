d0f4436b9962c97e4170bf251e58bc3f60f27545
Hebes <1607388033@qq.com>
2024-03-20 17:58:31 +0800

    1111

--- Assets/10万物品显示/SV/BagItem.cs
+++ Assets/10万物品显示/SV/BagItem.cs
@@ -1,24 +1,24 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-/// <summary>
-/// 格子类对象  他是放在背包里的一个一个的道具格子
-/// 主要用来显示 单组道具信息的
-/// </summary>
-public class BagItem : MonoBehaviour, IItemBase<Item>
-{
-    public Text text;
-
-    /// <summary>
-    /// 这个方法 是用于初始化 道具格子信息
-    /// </summary>
-    /// <param name="info"></param>
-    public void InitInfo(Item info)
-    {
-        //先读取道具表 
-        //根据表中数据 来更新信息
-        //更新图标
-        //更新名字
-        text.text = info.num.ToString();//更新道具数量
-    }
-}
+﻿// using UnityEngine;
+// using UnityEngine.UI;
+//
+// /// <summary>
+// /// 格子类对象  他是放在背包里的一个一个的道具格子
+// /// 主要用来显示 单组道具信息的
+// /// </summary>
+// public class BagItem : MonoBehaviour, IItemBase<Item>
+// {
+//     public Text text;
+//
+//     /// <summary>
+//     /// 这个方法 是用于初始化 道具格子信息
+//     /// </summary>
+//     /// <param name="info"></param>
+//     public void InitInfo(Item info)
+//     {
+//         //先读取道具表 
+//         //根据表中数据 来更新信息
+//         //更新图标
+//         //更新名字
+//         text.text = info.num.ToString();//更新道具数量
+//     }
+// }
---
+++ Assets/虚拟摇杆/摇杆资源/Script/CubeMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+/// <summary>
+/// 物体移动
+/// </summary>
+public class CubeMove : MonoBehaviour
+{
+    private Vector3 dir;
+
+    void Start()
+    {
+        JpyStickPanel.Instance.joystickTrigger = CheckDirChange;
+    }
+
+    void Update()
+    {
+        this.transform.Translate(dir * Time.deltaTime, Space.World);
+    }
+
+    private void CheckDirChange(Vector2 value)
+    {
+        this.dir.x = value.x;
+        this.dir.z = value.y;
+    }
+}
