a6e50f610bbb6702d6cf26971d8c03c35b694ddd
Hebes <1607388033@qq.com>
2024-04-28 17:48:54 +0800

    11111

--- Assets/Test/NttttewBehaviourScript.cs
+++ Assets/Test/NttttewBehaviourScript.cs
@@ -17,29 +17,29 @@ namespace MyNamespace
 
         private void Awake()
         {
-            //sb= new StringBuilder();
+            sb= new StringBuilder();
         }
 
         private void Update()
         {
             
-            Debug.Log(collider.bounds.max);
-            // Profiler.BeginSample($"StringRun");
-            // if (Input.GetKeyDown(KeyCode.A))
-            // {
-            //     for (var i = 0; i < 100; i++)
-            //         sb.Append("你好");
-            //     txt.text = sb.ToString();
-            // }
-            //
-            // if (Input.GetKeyDown(KeyCode.B))
-            // {
-            //     var sb1 = "你好";
-            //     for (var i = 0; i < 100; i++)
-            //         sb1 += sb1;
-            //     txt.text = sb1;
-            // }
-            // Profiler.EndSample();
+            //Debug.Log(collider.bounds.max);
+            Profiler.BeginSample($"StringRun");
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                for (var i = 0; i < 100; i++)
+                    sb.Append("你好");
+                txt.text = sb.ToString();
+            }
+            
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                var sb1 = "你好";
+                for (var i = 0; i < 100; i++)
+                    sb1 += sb1;
+                txt.text = sb1;
+            }
+            Profiler.EndSample();
         }
     }
 }
