9e61a6ce69519c5d3c2274d835a114b54608a03f
Hebes <1607388033@qq.com>
2024-05-06 18:06:13 +0800

    111

---
+++ Assets/SLG游戏画行军线/Resources/DrawLine/SetPropies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPropies : MonoBehaviour {
+    public Color m_color = new Color(1, 1, 1, 1);
+    public float m_speed = 1.0f;
+
+#if false
+    void Update () {
+        Transform transform = GetComponent<Transform>();
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        var vertices = mesh.vertices;
+        //颜色
+        var colors = new Color[vertices.Length];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = m_color;
+        }
+        mesh.colors = colors;
+
+        //Y轴缩放，速度值
+        var uv2 = new Vector2[vertices.Length];
+        for (int i = 0; i < uv2.Length; i++)
+        {
+            uv2[i] = new Vector2(transform.localScale.y / transform.localScale.x, m_speed);
+        }
+        mesh.uv2 = uv2;
+	}
+#endif
+}
