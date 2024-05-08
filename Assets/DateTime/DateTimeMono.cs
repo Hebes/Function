ba840996e2a07b4ce16fcc9706ae104b933d4097
Hebes <1607388033@qq.com>
2024-05-08 17:21:18 +0800

    11

---
+++ Assets/DateTime/DateTimeMono.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace DateTimett
+{
+    /// <summary>
+    /// https://blog.csdn.net/weixin_40948750/article/details/108293874
+    /// https://wenku.csdn.net/answer/5w49dqoq0y
+    /// </summary>
+    public class DateTimeMono : MonoBehaviour
+    {
+        private void Awake()
+        {
+           Debug.Log($"今天{DateTime.Now.Date.ToShortDateString()}");
+        }
+    }
+}
