a1677dd3e435e6a55db3445ff54196edfdbd9521
Hebes <1607388033@qq.com>
2024-05-14 15:36:42 +0800

    111

---
+++ Assets/日志/远程日志/RemoteLog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace 日志.远程日志
+{
+    /// <summary>
+    /// https://blog.csdn.net/linxinfa/article/details/119280053 游戏开发进阶】新发带你玩转Unity日志打印技巧（彩色日志 | 日志存储与上传 | 日志开关 | 日志双击溯源）
+    /// https://github.com/871041532/UnityConsoleTool python 日志
+    /// </summary>
+    public class RemoteLog : MonoBehaviour
+    {
+        
+    }
+}
