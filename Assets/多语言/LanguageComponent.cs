5cf524607ccaa4782e7e0f74e20bdc3eff20934c
Hebes <1607388033@qq.com>
2024-05-20 14:15:07 +0800

    修改多语言

--- Assets/多语言/LanguageComponent.cs
+++ Assets/多语言/LanguageComponent.cs
@@ -1,45 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
-namespace ACLanguage
+namespace Assets.多语言
 {
+    /// <summary>
+    /// 多语言组件
+    /// </summary>
+    [RequireComponent(typeof(Text))]
     public class LanguageComponent : MonoBehaviour
     {
-        public string key;
+        public Text text; //组件
+        public string key; //关键字
+        private LanguageManager languageManager;
 
-        private Text m_Text;
-        //private TMP_Text m_MeshText;
         private void Awake()
         {
-            m_Text = GetComponent<Text>();
-            //m_MeshText = GetComponent<TMP_Text>();
+            languageManager = LanguageManager.Instance;
+            languageManager.AddLanguageComponent(this);
         }
+        public void Change() => text.text = languageManager.Get(key);
 
-        private void OnEnable()
-        {
-            //如果是放在UI界面中的话开启这段代码
-            //OnSwitchLanguage();
-            //LanaguageBridge.Instance.OnLanguageChangeEvt += OnSwitchLanguage;
-        }
-        private void Start()
-        {
-            //一下代码在LanguageManagerAwake之后执行
-            //测试时候用的放在UI界面中的可以删除
-            OnSwitchLanguage();
-            LanguageBridge.Instance.OnLanguageChangeEvt += OnSwitchLanguage;
-        }
-
-        private void OnDisable()
-        {
-            LanguageBridge.Instance.OnLanguageChangeEvt -= OnSwitchLanguage;
-        }
-
-        private void OnSwitchLanguage()
-        {
-            if (m_Text != null)
-                m_Text.text = LanguageBridge.Instance.GetText(key);
-            //if (m_MeshText != null)
-                //m_MeshText.text = LanaguageBridge.Instance.GetText(key);
-        }
+        private void OnMouseOver() => text.text = key;
     }
 }
--- Assets/多语言/Test.cs
+++ Assets/多语言/Test.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.多语言
@@ -10,39 +8,30 @@ namespace Assets.多语言
         public Toggle toggle1; //中文
         public Toggle toggle2; //英文
 
-        private Dictionary<string, string> EnglishLanguageDic { get; set; } = new Dictionary<string, string>()
-        {
-            { "1", "Hello" },
-        };
-
-        private Dictionary<string, string> ChineseLanguageDic { get; set; } = new Dictionary<string, string>()
-        {
-            { "1", "你好" },
-        };
-
         private void Awake()
         {
-            // var languageManager = new LanguageManager();
-            // languageManager.Init();
-            //
-            // toggle1.onValueChanged.AddListener(Ontoggle1);
-            // toggle2.onValueChanged.AddListener((bool isOn) =>
-            // {
-            //     if (isOn)
-            //     {
-            //         Debug.Log("切换英文");
-            //         OnChangeLanuage(ELanguageMode.English);
-            //     }
-            // });
+            var languageManager = new LanguageManager();
+            languageManager.Init();
+
+            var languageData1 = new LanguageData
+            {
+                chinese = "你好",
+                english = "Hello"
+            };
+            languageManager.AddLanguageDataDicData(languageData1);
+            
+            toggle1.onValueChanged.AddListener(Ontoggle1);
+            toggle2.onValueChanged.AddListener((bool isOn) =>
+            {
+                if (!isOn) return;
+                languageManager.OnChangeLanguage(ELanguageMode.English);
+            });
         }
         
         private void Ontoggle1(bool isOn)
         {
-            // if (isOn)
-            // {
-            //     Debug.Log("切换中文");
-            //     OnChangeLanuage(ELanguageMode.Chinese);
-            // }
+            if (!isOn) return;
+            LanguageManager.Instance.OnChangeLanguage(ELanguageMode.Chinese);
         }
     }
 }
